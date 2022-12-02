21de8efb4739c34b7d4a4dce708d617a08fe9cca
Gayathri Gollangi <gayathrigollangii@gmail.com>
2022-12-02 09:42:18 +0000

    Super Over League

---
+++ index.js
@@ -0,0 +1,20 @@
+const element = (
+  //   Write your code here.
+  <div className="bg">
+    <h1 className="heading">Super Over League</h1>
+    <div>
+      <img
+        className="imgs"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      />
+      >
+      <img
+        className="imgs"
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      />
+      >
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
